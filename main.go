75c76c20070539b1f563e2592db78836e4b41b01
adobie <1093557432@qq.com>
2020-12-21 21:55:51 +0800

    新增websocket收发（无法广播）

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 func main(){
 	server.Database()
 	r:=gin.Default()
+	r.GET("/ws",func(c *gin.Context){
+		server.Handler(c)
+	})
 	r.Run()
 }
 
--- server/chat.go
+++ server/chat.go
@@ -1,1 +1,47 @@
 package server
+
+import "github.com/gin-gonic/gin"
+
+type Recvmsg struct{
+	Recieve string `json:"recieve"`
+}
+
+type Sendmsg struct{
+	Send string `json:"send"`
+}
+
+func Handler(c *gin.Context) {
+	var recv Recvmsg
+	var send Sendmsg
+	conn,err := Websocket.upgrader.Upgrade(c.Writer,c.Request,nil)
+    if err != nil{
+		panic(err)
+	}
+	NewMessage:=make(chan string)
+	go func() {
+	for {
+		err = conn.ReadJSON(&recv)
+		if err != nil {
+			close(NewMessage)
+			return
+		}
+        NewMessage<-recv.Recieve
+	}
+}()
+for{
+	select {
+	case msg,ok:=<-NewMessage:
+		if !ok{
+			return
+		}
+		send=Sendmsg{
+			Send:msg,
+		}
+		err=conn.WriteJSON(&send)
+		if err != nil {
+			return
+		}
+	}
+}
+
+}
